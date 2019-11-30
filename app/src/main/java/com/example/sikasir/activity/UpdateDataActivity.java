e4193fe2b61aa362270353c65a9d9b0a5e6fbadd
cantsleepbeforecode <samringo25@gmail.com>
2019-11-30 15:52:18 +0800

    v-4. final layout

--- app/src/main/java/com/example/sikasir/activity/UpdateDataActivity.java
+++ app/src/main/java/com/example/sikasir/activity/UpdateDataActivity.java
@@ -11,6 +11,6 @@ public class UpdateDataActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_update_data);
+        setContentView(R.layout.activity_list_barang);
     }
 }
