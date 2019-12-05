4a9646cd8ca8cbce7fb97f0a2ed0c2c8b4501095
cantsleepbeforecode <samringo25@gmail.com>
2019-12-06 01:48:45 +0800

    v10 - show some product using adapter

--- app/src/main/java/com/example/sikasir/activity/UpdateDataActivity.java
+++ app/src/main/java/com/example/sikasir/activity/UpdateDataActivity.java
@@ -1,16 +1,85 @@
 package com.example.sikasir.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.example.sikasir.R;
+import com.example.sikasir.adapter.ProductAdapter;
+import com.example.sikasir.database.ProductHelper;
+import com.example.sikasir.entity.Product;
+import com.google.android.material.snackbar.Snackbar;
 
-public class UpdateDataActivity extends AppCompatActivity {
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+
+public class UpdateDataActivity extends AppCompatActivity implements LoadDataCallback {
+    public static final String EXTRA_ID_PRODUCT = "extra_id_product";
+
+    private RecyclerView recyclerView;
+    private ProductAdapter adapter;
+    private ProductHelper helper;
+    private static final String EXTRA_STATE = "EXTRA_STATE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_barang);
+
+        recyclerView = findViewById(R.id.rv_product);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        adapter = new ProductAdapter(this);
+        recyclerView.setAdapter(adapter);
+
+        if (savedInstanceState == null) {
+            new LoadProductAsync(helper, this).execute();
+        }
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelableArrayList(EXTRA_STATE, adapter.getListProduct());
+    }
+
+    @Override
+    public void postExecute(ArrayList<Product> products) {
+        if (products.size() > 0) {
+            adapter.setListProduct(products);
+        } else {
+            adapter.setListProduct(new ArrayList<Product>());
+            Snackbar.make(recyclerView, "Tidak ada data saat ini", Snackbar.LENGTH_SHORT).show();
+        }
+    }
+
+    private static class LoadProductAsync extends AsyncTask<Void, Void, ArrayList<Product>> {
+        private final WeakReference<ProductHelper> weakProductHelper;
+        private final WeakReference<LoadDataCallback> weakCallback;
+
+        private LoadProductAsync(ProductHelper helper, LoadDataCallback callback) {
+            weakProductHelper = new WeakReference<>(helper);
+            weakCallback = new WeakReference<>(callback);
+        }
+
+        @Override
+        protected ArrayList<Product> doInBackground(Void... voids) {
+            return weakProductHelper.get().query();
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Product> products) {
+            super.onPostExecute(products);
+
+            weakCallback.get().postExecute(products);
+        }
+    }
+}
+
+interface LoadDataCallback{
+    void postExecute(ArrayList<Product> products);
 }
--- app/src/main/java/com/example/sikasir/adapter/ProductAdapter.java
+++ app/src/main/java/com/example/sikasir/adapter/ProductAdapter.java
@@ -1,5 +1,86 @@
 package com.example.sikasir.adapter;
 
-public class ProductAdapter {
+import android.app.Activity;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.sikasir.R;
+import com.example.sikasir.activity.UpdateDataActivity;
+import com.example.sikasir.database.ProductHelper;
+import com.example.sikasir.entity.Product;
+
+import java.util.ArrayList;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> {
+    private final ArrayList<Product> listProduct = new ArrayList<>();
+    private final Activity activity;
+
+    public ProductAdapter(Activity activity) {
+        this.activity = activity;
+    }
+
+    public ArrayList<Product> getListProduct() {
+        return listProduct;
+    }
+
+    public void setListProduct(ArrayList<Product> listProduct) {
+        if (listProduct.size() > 0) {
+            this.listProduct.clear();
+        }
+        this.listProduct.addAll(listProduct);
+
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_barang, parent, false);
+        return new ProductViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductViewHolder holder, final int position) {
+        holder.tvProduct.setText(String.format("%s %s", listProduct.get(position).getId(), listProduct.get(position).getName()));
+        holder.cvDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ProductHelper helper = new ProductHelper(activity.getApplicationContext());
+                helper.delete(listProduct.get(position).getId());
+            }
+        });
+
+        holder.cvUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(activity, UpdateDataActivity.class);
+                intent.putExtra(UpdateDataActivity.EXTRA_ID_PRODUCT, listProduct.get(position));
+                activity.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return listProduct.size();
+    }
+
+    class ProductViewHolder extends RecyclerView.ViewHolder {
+        final TextView tvProduct;
+        final CardView cvDelete, cvUpdate;
+
+        ProductViewHolder(View view) {
+            super(view);
+            tvProduct = view.findViewById(R.id.txt_product);
+            cvDelete = view.findViewById(R.id.cv_delete_product);
+            cvUpdate = view.findViewById(R.id.cv_update_product);
+        }
+    }
 }
